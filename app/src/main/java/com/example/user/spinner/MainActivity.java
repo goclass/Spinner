2c65c1a0fc192ca375f0864e8be1db13443887ff
goclass <yflan@nfu.edu.tw>
2017-07-11 10:53:06 +0800

    監聽選項內容何者被選取

--- app/src/main/java/com/example/user/spinner/MainActivity.java
+++ app/src/main/java/com/example/user/spinner/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.user.spinner;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.example.user.spinner.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
+    TextView textView;
+
     String[] fruit = new String[]{"香蕉","蘋果","鳳梨"};
 
 
@@ -18,11 +26,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         spinner = (Spinner)findViewById(R.id.spinner);
+        textView = (TextView)findViewById(R.id.textView);
 
         ArrayAdapter<String> adapterFruit = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,fruit);
 
         adapterFruit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(adapterFruit);
+
+        //監聽選項內容何者被選取
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, android.view.View view, int position, long id) {
+                textView.setText(parent.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
